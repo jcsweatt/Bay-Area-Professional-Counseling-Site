baaf8b38e9cc6cafe2739c9b55985baefeb55bcc
Christian <271591215+jcsweatt@users.noreply.github.com>
2026-06-28 22:53:15 -0700

    Refine Quiet Coast preview therapist photo crops

--- assets/quiet-coast-about.js
+++ assets/quiet-coast-about.js
@@ -140,7 +140,7 @@
     },
     {
       href: previewPath("tifani/"),
-      image: siteAsset("/assets/Tif-Pic-8b28f51f7639.JPG"),
+      image: siteAsset("/assets/Tif-pic-3-b87dc034dccb.jpg"),
       alt: "Tifani Nobles",
       name: "Tifani",
       note: "Anxiety, grief, family change, stress"
